0460a8410241bba257a70b5d3e71fa8d2dd0f617
Elramly <elramlm@147dda9519cc.ant.amazon.com>
2021-12-23 00:19:17 +0000

    Please work k6 now

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ export default function () {
   http.get('https://test.k6.io');
   sleep(1);
 }
-import http from 'k6/http';
-import { sleep } from 'k6';
-
-export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
-}
